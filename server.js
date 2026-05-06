673ec6e4c332704664e59597eb815ca860b6030c
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2026-05-06 09:35:45 +0700

    fix deploy render

--- server.js
+++ server.js
@@ -4,16 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Traffic Spike is running 🚀");
-});
-
-app.get("/ping", (req, res) => {
-  res.json({
-    message: "pong",
-    totalRequests: 10
-  });
+  res.send("CI/CD GitLab is working 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on " + PORT);
 });
