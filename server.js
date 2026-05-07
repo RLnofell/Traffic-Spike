0d9929477f91e4e2c8c11bc8447c5d9292ddf184
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2026-05-07 09:50:55 +0700

    morgan

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const helmet = require("helmet");
 const compression = require("compression");
+const morgan = require("morgan");
 const app = express();
 
 const PORT = process.env.PORT || 10000;
 
+app.use(morgan("dev"));
 app.use(helmet({
   contentSecurityPolicy: false,
 }));
